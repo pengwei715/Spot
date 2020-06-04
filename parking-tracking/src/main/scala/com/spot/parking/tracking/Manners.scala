1cc6841a30c7e8c2e8feffcf4fd6495b4e04b9da
pgw <pengwei715@gmail.com>
2020-06-03 23:32:17 -0700

    start to code scala

---
+++ parking-tracking/src/main/scala/com/spot/parking/tracking/Manners.scala
@@ -0,0 +1,12 @@
+package com.spot.parking.tracking
+
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions._
+
+object Manners {
+
+  def happyData()(df: DataFrame): DataFrame = {
+    df.withColumn("happy", lit("data is fun"))
+  }
+
+}
