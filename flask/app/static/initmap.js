066a249e161ce85b1b659d11a3e9f21a8bb5ed05
Peng Wei <weipeng0715@gmail.com>
2020-06-19 06:50:18 +0000

    finished all the front end

--- flask/app/static/initmap.js
+++ flask/app/static/initmap.js
@@ -1,16 +1,17 @@
 var CHI = new google.maps.LatLng(41.877009, -87.630047);
-
+caricon = 'http://maps.google.com/mapfiles/kml/pal2/icon47.png';
 
 function initialize() {
-  var mapOptions = { zoom: 13,
+  var mapOptions = { zoom: 14,
                      center: CHI,
                      streetViewControl: false };
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
   var marker = new google.maps.Marker({position: CHI,
-                                       title:    "Spot!"});
+					 zIndex:14,
+					 icon: caricon,
+					 title: "Spot!"});
   marker.setMap(map);
 }
-
 
 google.maps.event.addDomListener(window, 'load', initialize);
--- flask/app/static/map.js
+++ flask/app/static/map.js
@@ -1,57 +1,51 @@
-var CHI = new google.maps.LatLng(41.877009, -87.630047);
-var map;
-var marker = false;
-
-caricon = 'http://maps.google.com/mapfiles/kml/pal2/icon47.png';
-
+var marker;
+car = 'http://maps.google.com/mapfiles/kml/pal2/icon47.png';
 function initMap() {
-    var mapOptions = { zoom: 11,
-		       center: CHI,
-		       streetViewControl: false};
-    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var pos = new google.maps.LatLng(lat, lng);
+    var mapOptions = { zoom: 17,
+		       center: pos,
+		       streetViewControl: false };
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    marker = new google.maps.Marker({postition: pos,
+				     zIndex: 17,
+				     icon: car,
+				     title:"car"});
+    marker.setMap(map);
     
-    google.maps.event.addListener(map, 'click', function(event) {                
-        //Get the location that the user clicked.
-        var clickedLocation = event.latLng;
-        //If the marker hasn't been added.
-        if(marker === false){
-            //Create the marker.
-            marker = new google.maps.Marker({
-                position: clickedLocation,
-                map: map,
-                draggable: true //make it draggable
-            });
-            //Listen for drag events!
-            google.maps.event.addListener(marker, 'dragend', function(event){
-                markerLocation();
-            });
-        } else{
-            //Marker has already been added, so just change its location.
-            marker.setPosition(clickedLocation);
-        }
-        //Get the marker's location.
-        markerLocation();
+    var top = Number(lat) + 0.00125;
+    var bottom = Number(lat) - 0.00125;
+    var left = Number(lng) - 0.00125;
+    var right = Number(lng) + 0.00125;
+    
+    var rec = new google.maps.Rectangle({
+	strokeColor: color,
+	strokeOpacity: 0.8,
+	strokeWeight: 2,
+	fillColor: color,
+	fillOpacity: 0.35,
+	map:map,
+	bounds: {
+	    north: top,
+	    south: bottom,
+	    east: right,
+	    west: left
+	}
     });
-}
-        
-//This function will get the marker's current location and then add the lat/long
-//values to our textfields so that we can save the location.
-function markerLocation(){
-    //Get location.
-    var currentLocation = marker.getPosition();
-    //Add lat and lng values to a field that we can save.
-    document.getElementById('lat').value = currentLocation.lat(); //latitude
-    document.getElementById('lng').value = currentLocation.lng(); //longitude
-}
 
-function updateBlock() {
-    $.getJSON('/query',
-	      function(data) {
-		  
-	      }
+    rec.addListener('click', showData);
+    infoWindow = new google.maps.InfoWindow();
+    function showData(event) {
+	var ne = rec.getBounds().getNorthEast();
+	var contentString = '<b>Compare to average</b><br>' +
+            'Average: ' + avg + '<br>' + 'Count: ' +  count;
+
+	infoWindow.setContent(contentString)
+	infoWindow.setPosition(ne);
+	infoWindow.open(map);
+    }
     
-
 }
-        
+
+
 //Load the map when the page has finished loading.
 google.maps.event.addDomListener(window, 'load', initMap);
